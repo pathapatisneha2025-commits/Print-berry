dae6769ea99f2c19c8369a27f2eb7cac12f8f2f3
Sneha-datla <datla.sneha1@gmail.com>
2025-12-27 22:04:58 +0530

    changes made

--- src/components/CtaSection.js
+++ src/components/CtaSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function CTASection() {
   return (
@@ -14,8 +15,9 @@ export default function CTASection() {
         expert team.
       </p>
 
-      <button style={styles.ctaBtn}>Start Your Project →</button>
-    </section>
+ <Link to="/contact" style={styles.link}>
+        <button style={styles.ctaBtn}>Start Your Project →</button>
+      </Link>    </section>
   );
 }
     const styles = {
