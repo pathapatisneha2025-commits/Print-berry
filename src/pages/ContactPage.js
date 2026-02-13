96d011c9552f80b959a749b6c3b21c97b243a708
Sneha-datla <datla.sneha1@gmail.com>
2026-02-13 20:30:25 +0530

    changes made

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -17,7 +17,7 @@ export default function ContactPage() {
   }
 
   const paddingMap = {
-    mobile: "100px 20px 40px",
+    mobile: "100px 0px 40px",
     tablet: "80px 30px 60px",
     desktop: "0px 40px 80px",
   };
